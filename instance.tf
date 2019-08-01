9e32796bada1f85d928d1e178a2d0ec353fbfe51
daudmu21 <daudmuhudin@gmail.com>
2019-07-31 20:38:10 -0500

    add

--- instance.tf
+++ instance.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "web" {
-      ami = "ami-056ee704806822732"
-      instance_type = "t2.micro"
+      ami = "${var.ami}"
+      instance_type = "${var.instance_type}"
       key_name = "${aws_key_pair.terraform_april.key_name}"
       security_groups = ["${aws_security_group.sec1.name}"]
       user_data = "${file("userdata.sh")}"
