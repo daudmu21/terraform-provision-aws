0b1d748459c6b716f081546df31c7b44d2503cce
daudmu21 <daudmuhudin@gmail.com>
2019-07-31 20:10:10 -0500

    update

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    region  = "us-west-2"
+    region  = "us-west-1"
     bucket  = "elasticbeanstalk-us-west-1-340873333260"
     key     = "dev.tfstate"
     encrypt = true
